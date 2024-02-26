9bb393292982afa1a79a9cd63644a54e8a960379
juruijr@qq.com <juruijr@qq.com>
2024-02-26 23:13:00 +0800

    第二次提交： 1.需完成DB文件创建，自动读取xml里的表结构 2.需要完成读取注册信息并添加到db里 3.需动态添加接口地址参数等信息 4.需完成okhtpp，retrofit网络请求

---
+++ app/src/main/java/view/newView.java
@@ -0,0 +1,25 @@
+package view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class newView extends View {
+    public newView(Context context) {
+        super(context);
+    }
+
+    public newView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public newView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public newView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
